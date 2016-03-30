f8e5f6f9e5849cbc2862e050f572d564fc37654f
Michael Comella <michael.l.comella@gmail.com>
2016-03-29 23:05:24 -0700

    Add board ns with gen-initial.

---
+++ src/mastermind/board.clj
@@ -0,0 +1,6 @@
+(ns mastermind.board) ; TODO: gen-class?
+
+(defn gen-initial [row-count solution] ; TODO: Validate solution.
+  {:solution solution
+   :play-field (into [] (take row-count (repeat {:guesses []
+                                                 :matches []})))})
