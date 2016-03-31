16f1c84deb2e039765b9c194c2285fc6c7828c09
Michael Comella <michael.l.comella@gmail.com>
2016-03-31 09:43:02 -0700

    Add board-state and partially completed insert.

--- src/clojuremind/board.clj
+++ src/clojuremind/board.clj
@@ -1,6 +1,44 @@
 (ns clojuremind.board) ; TODO: gen-class?
 
-(defn gen-initial [row-count solution] ; TODO: Validate solution.
-  {:solution solution
-   :play-field (into [] (take row-count (repeat {:guesses []
-                                                 :matches []})))})
+(defmacro when-input-valid [input & body]
+  `(if (valid-input? ~input)
+     ~@body
+     (throw (IllegalArgumentException. (str "Invalid input " ~input)))))
+
+(defn- valid-input? [coll]
+  (and
+    (= 4 (count coll))
+    (every? integer? coll)))
+
+(defn gen-initial [row-count solution]
+  "Data structure of {... :play-field [{:guesses [...] :matches [...]} ...]}"
+  (when-input-valid
+    solution
+    {:solution solution
+     :play-field (into [] (take row-count (repeat {:guesses []
+                                                   :matches []})))}))
+
+(defn board-state [board] ; TODO: Better to store with board to prevent re-computation?
+  "Returns board state as :victory, :loss, or :in-progress"
+  (let [{solution :solution field :play-field} board]
+    (or (and (some #(= (:guesses %) solution) field)
+             :victory)
+        (and (every? #(not-empty (:guesses %)) field)
+             :loss)
+        :in-progress)))
+
+(defn- next-row-ind [board]
+  "Returns the index of the next empty row in guesses"
+  (as-> (:play-field board) field
+        (count (take-while #(not-empty (:guesses %)) field))))
+
+(defn- update-matches [ind board]
+  board)
+
+(defn insert-into [board input]
+  ; Assumes board is in the playing state. TODO: should I memoize and confirm that?
+  (when-input-valid
+    input
+    (let [next-row (next-row-ind board)
+          inserted-into-guesses (update-in board [:play-field next-row :guesses] #(into % input))]
+      (update-matches next-row-ind inserted-into-guesses))))
--- test/clojuremind/board_test.clj
+++ test/clojuremind/board_test.clj
@@ -2,17 +2,51 @@
   (:require [clojure.test :refer :all]
             [clojuremind.board :refer :all]))
 
+(def initial-solution (range 4))
+(def not-solution (repeat 4 0))
+(def initial-row-cnt 2)
+(def initial-board (gen-initial initial-row-cnt initial-solution))
+
 (deftest test-gen-initial
-  (testing "Generated initial board"
-    (let [row-cnt 12
-          solution [\R \G \B \R]
-          board (gen-initial row-cnt solution)]
+  (let [{actual-sol :solution field :play-field} initial-board
+        row-cnt initial-row-cnt]
+    (testing "Generated initial board"
       (testing "is correct size"
-        (is (= row-cnt (-> board
-                           :play-field
-                           count))))
+        (is (= row-cnt (count field))))
       (testing "contains no matches and guesses"
-        (let [merged-match-guess-rows (map #(flatten (vals %)) (:play-field board))]
+        (let [merged-match-guess-rows (map #(flatten (vals %)) field)]
           (is (every? #(empty? %) merged-match-guess-rows))))
       (testing "contains same solution as argument"
-        (is (= solution (:solution board)))))))
+        (is (= initial-solution actual-sol))))))
+
+(deftest test-insert-into
+  (testing "Inserted data throws when new row is"
+    (testing "too small"
+      (is (thrown? IllegalArgumentException (insert-into initial-board (range 3)))))
+    (testing "too large"
+      (is (thrown? IllegalArgumentException (insert-into initial-board (range 5)))))
+    (testing "not just numbers"
+      (is (thrown? IllegalArgumentException (insert-into initial-board [\R \G \B \R])))))
+  #_(testing "Play field is correct after inserting"
+    (let [i1 (repeat 4 1)
+          board-i1 (insert-into initial-board i1)
+          expected-field [initial-solution i1 []]]
+    (testing "one row"
+      (is (= (:play-field board-i1) expected-field))))))
+
+(deftest test-board-state
+  (testing "Board state is"
+    (testing "in progress when"
+      (testing "first generated"
+        (is (= :in-progress (board-state initial-board))))
+      (testing "non-winning/losing row inserted"
+        (is (= :in-progress (board-state (insert-into initial-board not-solution))))))
+    (testing "victory when solution is inserted"
+      (testing "first"
+        (is (= :victory (board-state (insert-into initial-board initial-solution)))))
+      (testing "last"
+        (as-> (insert-into (insert-into initial-board not-solution) initial-solution) last-sol-board
+              (is (= :victory (board-state last-sol-board))))))
+    (testing "loss when solution is not inserted"
+      (as-> (insert-into (insert-into initial-board not-solution) not-solution) board-loss
+            (is (= :loss (board-state board-loss)))))))
