354d2693b9296fddb334c72425f4a6383fddb021
Michael Comella <michael.l.comella@gmail.com>
2016-04-01 23:52:02 -0700

    Add simple main in core.

--- src/clojuremind/core.clj
+++ src/clojuremind/core.clj
@@ -1,7 +1,16 @@
 (ns clojuremind.core
+  (:require [clojuremind.board :as board-ns :refer [board-state]]
+            [clojuremind.io-cl :as cm.io])
   (:gen-class))
+
+(defn run-game [board]
+  (cm.io/pr-board board)
+  (condp = (board-state board)
+    :victory (println "You win!")
+    :loss (println "You lose!")
+    (run-game (board-ns/insert-into board (cm.io/prompt-for-row)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (run-game (board-ns/gen-initial 6 [0 0 0 0])))
