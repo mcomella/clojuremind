600dc7efe8edad8953950032605f4d10c62f157e
Michael Comella <michael.l.comella@gmail.com>
2016-04-02 17:05:37 -0700

    Add update-matches functionality.

--- src/clojuremind/board.clj
+++ src/clojuremind/board.clj
@@ -31,8 +31,36 @@
   "Returns the index of the next empty row in guesses"
   (count (take-while #(not-empty %) (:guesses board))))
 
-(defn- update-matches [ind board]
-  board)
+(defn- get-color-pos-match-cnt-and-leftovers [guesses sol]
+  "Returns {:color-pos-match-cnt <#> :leftover-guesses [...] :leftover-sol [...]} where leftovers
+  are the values that didn't have a position and color match. Takes one row of guesses & the solution row"
+  (reduce (fn [acc [guess sol]]
+            (if (= guess sol)
+              (update acc :color-pos-match-cnt inc)
+              (update (update acc :leftover-guesses conj guess)
+                      :leftover-sol conj sol)))
+          {:color-pos-match-cnt 0
+           :leftover-guesses []
+           :leftover-sol []}
+          (map vector guesses sol)))
+
+(defn- get-color-only-match-cnt [guess-to-cnt sol-to-cnt]
+  "Returns count of color only matches. Takes two maps of internal frequencies
+  (e.g. {0 2}; 0 appeared 2 times) after the perfect matches have been removed."
+  (reduce (fn [acc [internal cnt]] (+ acc (min cnt (get sol-to-cnt internal 0))))
+          0
+          guess-to-cnt))
+
+(defn- update-matches [ind board] ; TODO: This is not very intuitive at all... Helper fn?
+  (let [guesses (get (:guesses board) ind)
+        {color-pos-match-cnt :color-pos-match-cnt
+         leftover-guesses :leftover-guesses
+         leftover-sol :leftover-sol} (get-color-pos-match-cnt-and-leftovers guesses (:solution board))
+        color-only-match-cnt (get-color-only-match-cnt (frequencies leftover-guesses) (frequencies leftover-sol))
+
+        color-pos-match-seq (repeat color-pos-match-cnt 1)
+        color-only-match-seq (repeat color-only-match-cnt 0)]
+    (assoc-in board [:matches ind] (vec (into color-only-match-seq color-pos-match-seq)))))
 
 (defn insert-into [board input]
   ; Assumes board is in the playing state. TODO: should I memoize and confirm that?
